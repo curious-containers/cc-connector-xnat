5fda8ca917e4df8ee2c342a8ab9103d51e723c19
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2019-01-25 14:23:38 +0100

    --version

--- red_connector_xnat/main.py
+++ red_connector_xnat/main.py
@@ -1,6 +1,7 @@
 from cc_connector_cli.connector_cli import run_connector
 from red_connector_xnat.http import Http
+from red_connector_xnat.version import VERSION
 
 
 def main():
-    run_connector(Http)
+    run_connector(Http, version=VERSION)
